199a1678d679de3e41c31a57695067355c9cc81c
Ben Woodward <ben@benwoodward.me.uk>
2025-10-08 20:38:30 +0100

    chore: add and configure eslint/stylistic

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'eslint/config'
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import typescript from 'typescript-eslint'
+
+export default defineConfig(
+  stylistic.configs.recommended,
+
+  eslint.configs.recommended,
+
+  typescript.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  typescript.configs.stylisticTypeChecked,
+  {
+    files: ['**/*.(c|m)?js'],
+    extends: [typescript.configs.disableTypeChecked],
+  },
+)
